b3079c7f677af4ac280691c2c108eb5674fcb7fa
like432 <1968431069@qq.com>
2025-01-17 13:47:34 +0800

    STM32基础工程创建

---
+++ STM32 project/Project/Objects/main.d
@@ -0,0 +1,5 @@
+.\objects\main.o: user\main.c
+.\objects\main.o: .\Start\stm32f10x.h
+.\objects\main.o: .\Start\core_cm3.h
+.\objects\main.o: C:\Keil_v5\ARM\ARM Compiler 5.06 update 7\Bin\..\include\stdint.h
+.\objects\main.o: .\Start\system_stm32f10x.h
